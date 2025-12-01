01965b53f67c2a1d4107a9fb17531c1b7142b089
shu-htk <shu-htk@outlook.jp>
2025-12-01 16:32:34 +0900

    rename html/* en/*

--- script/make_html.sh
+++ script/make_html.sh
@@ -5,16 +5,16 @@ css=css/border.css
 tmp=tmp/tmp
 
 echo "generating directries ..."
-mkdir -p tmp html/ex html/ref
-if [ ! -e html/fig ] ; then
-    cp -rp $src/doc/fig html
-    ln -s html/fig .
+mkdir -p tmp en/ex en/ref
+if [ ! -e en/fig ] ; then
+    cp -rp $src/doc/fig en
+    ln -s en/fig .
     cd ..
-    ln -s tip-html/html/fig .
+    ln -s tip-html/en/fig .
     cd tip-html
 fi
 echo "generating index.html"
-cat $src/README.md | sed 's%doc/%html/%g' | sed 's%\.md%.html%g' > tip
+cat $src/README.md | sed 's%doc/%en/%g' | sed 's%\.md%.html%g' > tip
 pandoc -f markdown -t html tip -s --self-contained -c $css -o index.html \
        >& /dev/null
 rm tip
@@ -22,7 +22,7 @@ rm tip
 for f in $src/doc/*.md $src/doc/ex/*.md $src/doc/ref/*.md ; do
     f2=$(basename ${f%.*})
     cat $f | sed 's%\.md%.html%g' | sed 's%\[@\]%[ @ ]%g' > $f2
-    dest=$(echo ${f%.*}.html | sed "s%$src/doc%html%g")
+    dest=$(echo ${f%.*}.html | sed "s%$src/doc%en%g")
     echo "generating $dest"
 #    echo $f $f2 $dest
     pandoc -f markdown -t html $f2 -s --self-contained -c $css -o $tmp \
